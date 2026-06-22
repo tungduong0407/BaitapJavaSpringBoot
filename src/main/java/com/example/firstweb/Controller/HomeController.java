a481dbf48f97ed93163624ff668abb08350969a8
Vu Tung Duong <vutungduong086@gmail.com>
2026-06-22 11:32:51 +0700

    Initial Spring Boot project

---
+++ src/main/java/com/example/firstweb/Controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.firstweb.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Hello Spring Boot";
+    }
+}
